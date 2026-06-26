c52fd05c1db83244de1912135bf92ad80c373c70
Radhika kapoor <radhikakapoor383@gmail.com>
2026-06-27 02:16:45 +0500

    feat: add typr module for import export and created a new app.js file

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,10 @@
+import {a} from './app.js'
 let h1 = document.createElement('h1')
 h1.innerHTML = 'Heyy JS'
 console.log(h1)
 document.body.appendChild(h1)
+
+let h2 = document.createElement('h1')
+h2.innerHTML = a
+console.log(h2)
+document.body.appendChild(h2)
