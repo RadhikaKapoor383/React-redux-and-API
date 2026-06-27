41fd6e405867e070956b3530aa0f47197264e9ab
Radhika kapoor <radhikakapoor383@gmail.com>
2026-06-28 03:26:00 +0500

    feat: a separate component folder

---
+++ Components/src/App.jsx
@@ -0,0 +1,10 @@
+import Card from "./components/card.jsx";
+import Navbar from "./components/navbar.jsx";
+function App() {
+    return (
+    <>
+        <Navbar />
+        <Card />
+    </>)
+}
+export default App;
