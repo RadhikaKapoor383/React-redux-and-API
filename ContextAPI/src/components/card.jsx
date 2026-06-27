da9e482e9d74c9e71c7447e7a1a49463451829df
Radhika kapoor <radhikakapoor383@gmail.com>
2026-06-28 02:58:20 +0500

    creating component based architecture

---
+++ ContextAPI/src/components/card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function Card() {
+    return (
+    <>
+        <div className='card'>
+            <h1>Radhika Kapoor</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error nulla, odio, recusandae aperiam maiores, dicta ullam veniam libero placeat repellendus deleniti. Quisquam nulla autem, illum velit corrupti commodi quod ea.</p>
+        </div>
+    </>)
+}
+export default Card;
